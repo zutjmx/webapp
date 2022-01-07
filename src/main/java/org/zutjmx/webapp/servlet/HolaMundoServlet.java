dcb07f22074ae1284ce3c97a0646b374630b565d
Jesus Zuñiga <jesuszt@hotmail.com>
2022-01-07 12:53:29 -0600

    Fin de la sección 3

---
+++ src/main/java/org/zutjmx/webapp/servlet/HolaMundoServlet.java
@@ -0,0 +1,34 @@
+package org.zutjmx.webapp.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/hola-mundo")
+public class HolaMundoServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        out.println("<!DOCTYPE html>");
+        out.println("<html>");
+        out.println("    <head>");
+        out.println("      <meta charset=\"UTF-8\">");
+        out.println("      <title>Hola Mundo Servlet</title>");
+        out.println("    </head>");
+        out.println("  <body>");
+        out.println("      <h1>Hola, hoy es " + new java.util.Date() + "</h1>");
+        out.println("  </body>");
+        out.println("</html>");
+        out.close();
+
+    }
+}
